aaa92de81a88c7c63368a7178373d767d7e15f37
MuzaffarHussain <zaffarhussain1997122@gmail.com>
2020-06-24 02:19:21 +0500

    Changes in Javascript Assignment 6 is ready for update

--- app.js
+++ app.js
@@ -2,3 +2,38 @@
 // 1. Write a program that takes two user inputs for first and
 // last name using prompt and merge them in a new variable
 // titled fullName. Greet the user using his full name.
+// var firstNAME=prompt("Enter Your First Name: ");
+// var lastNAME=prompt("Enter Your Last Name: ");
+// var NAME=firstNAME+" "+lastNAME;
+// document.write("<h3>")
+// document.write("Hello "+NAME)
+// document.write("</h3>")
+
+
+
+//Chapter:21-25 Task#2
+// 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+// var favORiTE=prompt("Enter Your favorite mobile phone model1");
+// var str=favORiTE.length;
+// document.write("<h2>")
+// document.write("My Favorite phone is: " + favORiTE+"<br>")
+// document.write("Length of string: "+ str)
+// document.write("</h2>")
+
+
+
+
+
+//Chapter:21-25 Task#3
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+var str1='name';
+var str2=str1.split();
+document.write("String: "+str1)
+document.write("Index of 'n': "+st2)
+
+
+
+
